78343c36ddf1cf969ac0766d76595c1a4ec98e3f
yousef.joe175 <yousef.joe175@gmail.com>
2021-10-02 18:28:00 +0200

    added the Timer driver

--- 02 - MCAL/08- TIMER/TIMER_program.c
+++ 02 - MCAL/08- TIMER/TIMER_program.c
@@ -294,171 +294,296 @@ void MTIMER_voidSetIntervalPeriodic(u8 Copy_u8TimerModule, u16 Copy_u16PeriodinM
 }
 
 void MTIMER_voidGetInputCapture(u8 Copy_u8TimerModule, /*u8 Copy_u8Channel,*/ u8 Copy_u8Edge, u32* ptr_u32Buffer){
+	u32 Data;
 	if(Copy_u8TimerModule == 2){
-		#define LOCAL_TIMER				TIMER2
-		#define LOCAL_TIMER_LED_PIN		TIMER2_LED_PIN
+		/*set LED_PIN as AFPP */
+		MGPIO_voidSetPinMode(TIMER2_LED_PIN, OUTPUT_2MHZ_AF_PP);
+		/* set the channel as input with 8 samples per input */
+		SET_VAL(TIMER2->CCMR1,0,1);
+		SET_VAL(TIMER2->CCMR1,4,3);
+		/* set the edge of the input capture */
+		if(Copy_u8Edge == 1){
+			CLR_BIT(TIMER2->CCER,1);
+		}else if(Copy_u8Edge == 0){
+			SET_BIT(TIMER2->CCER,1);
+		}
+		/*enable the capture */
+		SET_BIT(TIMER2->CCER,0);
+		/*enable the timer*/
+		TIMER2->PSC = 8000;
+		TIMER2->CNT = 0;
+		SET_BIT(TIMER2->CR1,0);
+		while(GET_BIT(TIMER2->SR,1) == 0);
+		CLR_BIT(TIMER2->SR,1);
+		Data = TIMER2->CCR1;
+		*ptr_u32Buffer = Data;
 	}
 	else if (Copy_u8TimerModule == 3){
-		#define LOCAL_TIMER				TIMER3
-		#define LOCAL_TIMER_LED_PIN		TIMER3_LED_PIN
+		/*set LED_PIN as AFPP */
+		MGPIO_voidSetPinMode(TIMER3_LED_PIN, OUTPUT_2MHZ_AF_PP);
+		/* set the channel as input with 8 samples per input */
+		SET_VAL(TIMER3->CCMR1,0,1);
+		SET_VAL(TIMER3->CCMR1,4,3);
+		/* set the edge of the input capture */
+		if(Copy_u8Edge == 1){
+			CLR_BIT(TIMER3->CCER,1);
+		}else if(Copy_u8Edge == 0){
+			SET_BIT(TIMER3->CCER,1);
+		}
+		/*enable the capture */
+		SET_BIT(TIMER3->CCER,0);
+		/*enable the timer*/
+		TIMER3->PSC = 8000;
+		TIMER3->CNT = 0;
+		SET_BIT(TIMER3->CR1,0);
+		while(GET_BIT(TIMER3->SR,1) == 0);
+		CLR_BIT(TIMER3->SR,1);
+		Data = TIMER3->CCR1;
+		*ptr_u32Buffer = Data;
+		
 	}
 	else if (Copy_u8TimerModule == 4){
-		#define LOCAL_TIMER				TIMER4
-		#define LOCAL_TIMER_LED_PIN		TIMER4_LED_PIN
+		/*set LED_PIN as AFPP */
+		MGPIO_voidSetPinMode(TIMER4_LED_PIN, OUTPUT_2MHZ_AF_PP);
+		/* set the channel as input with 8 samples per input */
+		SET_VAL(TIMER4->CCMR1,0,1);
+		SET_VAL(TIMER4->CCMR1,4,3);
+		/* set the edge of the input capture */
+		if(Copy_u8Edge == 1){
+			CLR_BIT(TIMER4->CCER,1);
+		}else if(Copy_u8Edge == 0){
+			SET_BIT(TIMER4->CCER,1);
+		}
+		/*enable the capture */
+		SET_BIT(TIMER4->CCER,0);
+		/*enable the timer*/
+		TIMER4->PSC = 8000;
+		TIMER4->CNT = 0;
+		SET_BIT(TIMER4->CR1,0);
+		while(GET_BIT(TIMER4->SR,1) == 0);
+		CLR_BIT(TIMER4->SR,1);
+		Data = TIMER4->CCR1;
+		*ptr_u32Buffer = Data;
 	}
-	u32 Data;
-	/*set LED_PIN as AFPP */
-	MGPIO_voidSetPinMode(LOCAL_TIMER_LED_PIN, OUTPUT_2MHZ_AF_PP);
-	/* set the channel as input with 8 samples per input */
-	SET_VAL(LOCAL_TIMER->CCMR1,0,1);
-	SET_VAL(LOCAL_TIMER->CCMR1,4,3);
-	/* set the edge of the input capture */
-	if(Copy_u8Edge == 1){
-		CLR_BIT(LOCAL_TIMER->CCER,1);
-	}else if(Copy_u8Edge == 0){
-		SET_BIT(LOCAL_TIMER->CCER,1);
-	}
-	/*enable the capture */
-	SET_BIT(LOCAL_TIMER->CCER,0);
-	/*enable the timer*/
-	LOCAL_TIMER->PSC = 8000;
-	LOCAL_TIMER->CNT = 0;
-	SET_BIT(LOCAL_TIMER->CR1,0);
-	while(GET_BIT(LOCAL_TIMER->SR,1) == 0);
-	CLR_BIT(LOCAL_TIMER->SR,1);
-	Data = LOCAL_TIMER->CCR1;
-	*ptr_u32Buffer = Data;
+	
+	
 }
 
 
 void MTIMER_voidGetICFreq(u8 Copy_u8TimerModule, ICU_type *ptr_ICU_tBuffer ){
-	if(Copy_u8TimerModule == 2){
-		#define LOCAL_TIMER				TIMER2
-		#define LOCAL_TIMER_LED_PIN		TIMER2_LED_PIN
-	}
-	else if (Copy_u8TimerModule == 3){
-		#define LOCAL_TIMER				TIMER3
-		#define LOCAL_TIMER_LED_PIN		TIMER3_LED_PIN
-	}
-	else if (Copy_u8TimerModule == 4){
-		#define LOCAL_TIMER				TIMER4
-		#define LOCAL_TIMER_LED_PIN		TIMER4_LED_PIN
-	}
 	volatile u16 T1;
 	volatile u16 period;
-	/*set LED_PIN as AFPP */
-	MGPIO_voidSetPinMode(LOCAL_TIMER_LED_PIN, OUTPUT_2MHZ_AF_PP);
-	/*set channel1 to T1 and channel2 to T1 as well*/
-	SET_VAL(LOCAL_TIMER->CCMR1,0,1);
-	SET_VAL(LOCAL_TIMER->CCMR1,8,2);
-	/* set the input filter to 8 samples */
-	SET_VAL(LOCAL_TIMER->CCMR1,4,3);
-	SET_VAL(LOCAL_TIMER->CCMR1,12,3);
-	/* the polarity of channel1 is rising edge and channel2 is falling edge */
-	CLR_BIT(LOCAL_TIMER->CCER,1);
-	SET_BIT(LOCAL_TIMER->CCER,5);
-	/*enable the capture*/
-	SET_BIT(LOCAL_TIMER->CCER,0);
-	SET_BIT(LOCAL_TIMER->CCER,4);
-	/*enable the timer */
-	LOCAL_TIMER->PSC = 8000;
-	SET_BIT(LOCAL_TIMER->CR1,0);
-	while(GET_BIT(LOCAL_TIMER->SR, 1) == 0);
-	LOCAL_TIMER->CNT = 0;
-	CLR_BIT(LOCAL_TIMER->SR,1);
-	CLR_BIT(LOCAL_TIMER->SR,2);
-	while(GET_BIT(LOCAL_TIMER->SR, 2) == 0);
-	T1 = LOCAL_TIMER->CCR2;
-	while(GET_BIT(LOCAL_TIMER->SR,1) == 0);
-	period = LOCAL_TIMER->CCR1;
-	ptr_ICU_tBuffer->dutyCyle = T1*100/period;
-	ptr_ICU_tBuffer->freq = 1000.0f/period;
+	if(Copy_u8TimerModule == 2){
+		/*set LED_PIN as AFPP */
+		MGPIO_voidSetPinMode(TIMER2_LED_PIN, OUTPUT_2MHZ_AF_PP);
+		/*set channel1 to T1 and channel2 to T1 as well*/
+		SET_VAL(TIMER2->CCMR1,0,1);
+		SET_VAL(TIMER2->CCMR1,8,2);
+		/* set the input filter to 8 samples */
+		SET_VAL(TIMER2->CCMR1,4,3);
+		SET_VAL(TIMER2->CCMR1,12,3);
+		/* the polarity of channel1 is rising edge and channel2 is falling edge */
+		CLR_BIT(TIMER2->CCER,1);
+		SET_BIT(TIMER2->CCER,5);
+		/*enable the capture*/
+		SET_BIT(TIMER2->CCER,0);
+		SET_BIT(TIMER2->CCER,4);
+		/*enable the timer */
+		TIMER2->PSC = 8000;
+		SET_BIT(TIMER2->CR1,0);
+		while(GET_BIT(TIMER2->SR, 1) == 0);
+		TIMER2->CNT = 0;
+		CLR_BIT(TIMER2->SR,1);
+		CLR_BIT(TIMER2->SR,2);
+		while(GET_BIT(TIMER2->SR, 2) == 0);
+		T1 = TIMER2->CCR2;
+		while(GET_BIT(TIMER2->SR,1) == 0);
+		period = TIMER2->CCR1;
+		ptr_ICU_tBuffer->dutyCyle = T1*100/period;
+		ptr_ICU_tBuffer->freq = 1000.0f/period;
+	}
+	else if (Copy_u8TimerModule == 3){
+		/*set LED_PIN as AFPP */
+		MGPIO_voidSetPinMode(TIMER3_LED_PIN, OUTPUT_2MHZ_AF_PP);
+		/*set channel1 to T1 and channel2 to T1 as well*/
+		SET_VAL(TIMER3->CCMR1,0,1);
+		SET_VAL(TIMER3->CCMR1,8,2);
+		/* set the input filter to 8 samples */
+		SET_VAL(TIMER3->CCMR1,4,3);
+		SET_VAL(TIMER3->CCMR1,12,3);
+		/* the polarity of channel1 is rising edge and channel2 is falling edge */
+		CLR_BIT(TIMER3->CCER,1);
+		SET_BIT(TIMER3->CCER,5);
+		/*enable the capture*/
+		SET_BIT(TIMER3->CCER,0);
+		SET_BIT(TIMER3->CCER,4);
+		/*enable the timer */
+		TIMER3->PSC = 8000;
+		SET_BIT(TIMER3->CR1,0);
+		while(GET_BIT(TIMER3->SR, 1) == 0);
+		TIMER3->CNT = 0;
+		CLR_BIT(TIMER3->SR,1);
+		CLR_BIT(TIMER3->SR,2);
+		while(GET_BIT(TIMER3->SR, 2) == 0);
+		T1 = TIMER3->CCR2;
+		while(GET_BIT(TIMER3->SR,1) == 0);
+		period = TIMER3->CCR1;
+		ptr_ICU_tBuffer->dutyCyle = T1*100/period;
+		ptr_ICU_tBuffer->freq = 1000.0f/period;
+	}
+	else if (Copy_u8TimerModule == 4){
+		/*set LED_PIN as AFPP */
+		MGPIO_voidSetPinMode(TIMER4_LED_PIN, OUTPUT_2MHZ_AF_PP);
+		/*set channel1 to T1 and channel2 to T1 as well*/
+		SET_VAL(TIMER4->CCMR1,0,1);
+		SET_VAL(TIMER4->CCMR1,8,2);
+		/* set the input filter to 8 samples */
+		SET_VAL(TIMER4->CCMR1,4,3);
+		SET_VAL(TIMER4->CCMR1,12,3);
+		/* the polarity of channel1 is rising edge and channel2 is falling edge */
+		CLR_BIT(TIMER4->CCER,1);
+		SET_BIT(TIMER4->CCER,5);
+		/*enable the capture*/
+		SET_BIT(TIMER4->CCER,0);
+		SET_BIT(TIMER4->CCER,4);
+		/*enable the timer */
+		TIMER4->PSC = 8000;
+		SET_BIT(TIMER4->CR1,0);
+		while(GET_BIT(TIMER4->SR, 1) == 0);
+		TIMER4->CNT = 0;
+		CLR_BIT(TIMER4->SR,1);
+		CLR_BIT(TIMER4->SR,2);
+		while(GET_BIT(TIMER4->SR, 2) == 0);
+		T1 = TIMER4->CCR2;
+		while(GET_BIT(TIMER4->SR,1) == 0);
+		period = TIMER4->CCR1;
+		ptr_ICU_tBuffer->dutyCyle = T1*100/period;
+		ptr_ICU_tBuffer->freq = 1000.0f/period;
+	}
+	
+	
 }
 
 void MTIMER_voidSetPWM(u8 Copy_u8TimerModule, f32 Copy_f32Frequency, u8 Copy_u8DutyCycle){
 	if(Copy_u8TimerModule == 2){
-		#define LOCAL_TIMER				TIMER2
-		#define LOCAL_TIMER_LED_PIN		TIMER2_LED_PIN
+		MGPIO_voidSetPinMode(TIMER2_LED_PIN,OUTPUT_2MHZ_AF_PP);
+		/*set Capture compare mode as PWM 1 */
+		SET_VAL(TIMER2->CCMR1,4,6);
+		/* set the compare preload*/
+		SET_BIT(TIMER2->CCMR1,3);
+		/* set the auto reload preload */
+		SET_BIT(TIMER2->CR1,7);
+		/*set the direction as upcounter */
+		CLR_BIT(TIMER2->CR1,4);
+		/* set the compare output as active high */
+		CLR_BIT(TIMER2->CCER,1);
+		/* set the timer compare and auto reload */
+		TIMER2->PSC = 800;
+		TIMER2->ARR = 10000/Copy_f32Frequency;
+		TIMER2->CCR1 = Copy_u8DutyCycle * TIMER2->ARR / 100;
+		TIMER2->CNT = 0;
+		/* set the counter and set the update generator */
+		SET_BIT(TIMER2->CCER,0);
+		SET_BIT(TIMER2->CR1,0);
+		SET_BIT(TIMER2->EGR,0);
 	}
 	else if (Copy_u8TimerModule == 3){
-		#define LOCAL_TIMER				TIMER3
-		#define LOCAL_TIMER_LED_PIN		TIMER3_LED_PIN
+		MGPIO_voidSetPinMode(TIMER3_LED_PIN,OUTPUT_2MHZ_AF_PP);
+		/*set Capture compare mode as PWM 1 */
+		SET_VAL(TIMER3->CCMR1,4,6);
+		/* set the compare preload*/
+		SET_BIT(TIMER3->CCMR1,3);
+		/* set the auto reload preload */
+		SET_BIT(TIMER3->CR1,7);
+		/*set the direction as upcounter */
+		CLR_BIT(TIMER3->CR1,4);
+		/* set the compare output as active high */
+		CLR_BIT(TIMER3->CCER,1);
+		/* set the timer compare and auto reload */
+		TIMER3->PSC = 800;
+		TIMER3->ARR = 10000/Copy_f32Frequency;
+		TIMER3->CCR1 = Copy_u8DutyCycle * TIMER3->ARR / 100;
+		TIMER3->CNT = 0;
+		/* set the counter and set the update generator */
+		SET_BIT(TIMER3->CCER,0);
+		SET_BIT(TIMER3->CR1,0);
+		SET_BIT(TIMER3->EGR,0);
+
 	}
 	else if (Copy_u8TimerModule == 4){
-		#define LOCAL_TIMER				TIMER4
-		#define LOCAL_TIMER_LED_PIN		TIMER4_LED_PIN
+		MGPIO_voidSetPinMode(TIMER4_LED_PIN,OUTPUT_2MHZ_AF_PP);
+		/*set Capture compare mode as PWM 1 */
+		SET_VAL(TIMER4->CCMR1,4,6);
+		/* set the compare preload*/
+		SET_BIT(TIMER4->CCMR1,3);
+		/* set the auto reload preload */
+		SET_BIT(TIMER4->CR1,7);
+		/*set the direction as upcounter */
+		CLR_BIT(TIMER4->CR1,4);
+		/* set the compare output as active high */
+		CLR_BIT(TIMER4->CCER,1);
+		/* set the timer compare and auto reload */
+		TIMER4->PSC = 800;
+		TIMER4->ARR = 10000/Copy_f32Frequency;
+		TIMER4->CCR1 = Copy_u8DutyCycle * TIMER4->ARR / 100;
+		TIMER4->CNT = 0;
+		/* set the counter and set the update generator */
+		SET_BIT(TIMER4->CCER,0);
+		SET_BIT(TIMER4->CR1,0);
+		SET_BIT(TIMER4->EGR,0);
 	}
 	
-	MGPIO_voidSetPinMode(LOCAL_TIMER_LED_PIN,OUTPUT_2MHZ_AF_PP);
-	/*set Capture compare mode as PWM 1 */
-	SET_VAL(LOCAL_TIMER->CCMR1,4,6);
-	/* set the compare preload*/
-	SET_BIT(LOCAL_TIMER->CCMR1,3);
-	/* set the auto reload preload */
-	SET_BIT(LOCAL_TIMER->CR1,7);
-	/*set the direction as upcounter */
-	CLR_BIT(LOCAL_TIMER->CR1,4);
-	/* set the compare output as active high */
-	CLR_BIT(LOCAL_TIMER->CCER,1);
-	/* set the timer compare and auto reload */
-	LOCAL_TIMER->PSC = 800;
-	LOCAL_TIMER->ARR = 10000/Copy_f32Frequency;
-	LOCAL_TIMER->CCR1 = Copy_u8DutyCycle * LOCAL_TIMER->ARR / 100;
-	LOCAL_TIMER->CNT = 0;
-	/* set the counter and set the update generator */
-	SET_BIT(LOCAL_TIMER->CCER,0);
-	SET_BIT(LOCAL_TIMER->CR1,0);
-	SET_BIT(LOCAL_TIMER->EGR,0);
-
 }
 
 
 void MTIMER_voidStopTimer(u8 Copy_u8TimerModule){
 	if(Copy_u8TimerModule == 2){
-		#define LOCAL_TIMER				TIMER2
-		#define LOCAL_TIMER_LED_PIN		TIMER2_LED_PIN
+		CLR_BIT(TIMER2->CR1,0);
 	}
 	else if (Copy_u8TimerModule == 3){
-		#define LOCAL_TIMER				TIMER3
-		#define LOCAL_TIMER_LED_PIN		TIMER3_LED_PIN
+		CLR_BIT(TIMER3->CR1,0);
 	}
 	else if (Copy_u8TimerModule == 4){
-		#define LOCAL_TIMER				TIMER4
-		#define LOCAL_TIMER_LED_PIN		TIMER4_LED_PIN
+		CLR_BIT(TIMER4->CR1,0);
 	}
-	CLR_BIT(LOCAL_TIMER,0);
+	else
+	{
+		CLR_BIT(TIMER2->CR1,0);
+	}
 }
 
 u32 MTIMER_u32GetElapsedTime(u8 Copy_u8TimerModule){
+
 	if(Copy_u8TimerModule == 2){
-		#define LOCAL_TIMER				TIMER2
-		#define LOCAL_TIMER_LED_PIN		TIMER2_LED_PIN
+		return TIMER2->CNT * TIMER2->PSC;
 	}
 	else if (Copy_u8TimerModule == 3){
-		#define LOCAL_TIMER				TIMER3
-		#define LOCAL_TIMER_LED_PIN		TIMER3_LED_PIN
+		return TIMER3->CNT * TIMER3->PSC;
 	}
 	else if (Copy_u8TimerModule == 4){
-		#define LOCAL_TIMER				TIMER4
-		#define LOCAL_TIMER_LED_PIN		TIMER4_LED_PIN
+		return TIMER4->CNT * TIMER4->PSC;	
 	}
-	return LOCAL_TIMER->CNT * LOCAL_TIMER->PSC;
+	else {
+		return TIMER2->CNT * TIMER2->PSC;
+	}
 }
+
 u32 MTIMER_u32GetRemainingTime(u8 Copy_u8TimerModule){
 	if(Copy_u8TimerModule == 2){
-		#define LOCAL_TIMER				TIMER2
-		#define LOCAL_TIMER_LED_PIN		TIMER2_LED_PIN
+		return (TIMER2->ARR - TIMER2->CNT) * TIMER2->PSC; 	
 	}
 	else if (Copy_u8TimerModule == 3){
-		#define LOCAL_TIMER				TIMER3
-		#define LOCAL_TIMER_LED_PIN		TIMER3_LED_PIN
+		return (TIMER3->ARR - TIMER3->CNT) * TIMER3->PSC; 
+	
 	}
 	else if (Copy_u8TimerModule == 4){
-		#define LOCAL_TIMER				TIMER4
-		#define LOCAL_TIMER_LED_PIN		TIMER4_LED_PIN
+		return (TIMER4->ARR - TIMER4->CNT) * TIMER4->PSC; 
 	}
-	return (LOCAL_TIMER->ARR - LOCAL_TIMER->CNT) * LOCAL_TIMER->PSC; 
+	else {
+		return (TIMER2->ARR - TIMER2->CNT) * TIMER2->PSC;
+	}
 }
+
 
 void TIM2_IRQHandler(void){
 	if(TIMER2_periodFlag == 0){
